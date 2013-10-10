49cef9dee3a2515af9e32036aebb0ea7ac639087
BennyDalby <bdalbythoomkuzhy@b4-d-4cxcvq1.gdnindia.com>
2013-10-10 13:11:26 +0530

    initial commit

---
+++ TableScreen/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TableScreen
+//
+//  Created by b.dalby.thoomkuzhy on 10/7/13.
+//  Copyright (c) 2013 b.dalby.thoomkuzhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TableScreen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableScreen
+//
+//  Created by b.dalby.thoomkuzhy on 10/7/13.
+//  Copyright (c) 2013 b.dalby.thoomkuzhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
